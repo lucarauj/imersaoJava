7a3bcb26982c2cbfb2fed77d6a1cbf0d7b6a05fe
lucarauj <lukas.martins193@gmail.com>
2022-10-11 19:34:59 -0300

    Geradora de Figurinhas

---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+
+    public void cria() throws Exception {
+
+        // InputStream imagemOriginal = new FileInputStream();
+        // InputStream inputStream =
+        // new
+        // URL("https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@.jpg").openStream();
+
+        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.BLUE);
+        graphics.setFont(fonte);
+
+        graphics.drawString("TEXTO", 50, novaAltura - 100);
+
+        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        var geradora = new GeradoraDeFigurinhas();
+        geradora.cria();
+    }
+}
